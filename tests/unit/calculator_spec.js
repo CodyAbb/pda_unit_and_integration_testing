f8405e715d66b96d61b5681d96c3cbd9a7b4d453
Cody Abbott <cody.abbott16@gmail.com>
2020-02-25 13:30:51 +0000

    passing both sets of tests

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -13,65 +13,61 @@ describe("calculator", function() {
 
   it("can add one to four to get five", function() {
     calculator.numberClick(1);
-    calculator.operatorClick('+');
+    calculator.operatorClick("+");
     calculator.numberClick(4);
-    calculator.operatorClick('=');
+    calculator.operatorClick("=");
     assert.equal(5, calculator.runningTotal);
   });
 
   it("can subtract four from seven equals three", function() {
     calculator.numberClick(7);
-    calculator.operatorClick('-');
+    calculator.operatorClick("-");
     calculator.numberClick(4);
-    calculator.operatorClick('=');
+    calculator.operatorClick("=");
     assert.equal(3, calculator.runningTotal);
   });
   //
-  it("can multiply 3 by 5", function(){
+  it("can multiply 3 by 5", function() {
     calculator.numberClick(3);
-    calculator.operatorClick('*');
+    calculator.operatorClick("*");
     calculator.numberClick(5);
-    calculator.operatorClick('=');
+    calculator.operatorClick("=");
     assert.equal(15, calculator.runningTotal);
   });
 
-  it("can divide 21 by 7 to get 3", function(){
+  it("can divide 21 by 7 to get 3", function() {
     calculator.numberClick(2);
     calculator.numberClick(1);
-    calculator.operatorClick('/');
+    calculator.operatorClick("/");
     calculator.numberClick(7);
-    calculator.operatorClick('=');
+    calculator.operatorClick("=");
     assert.equal(3, calculator.runningTotal);
   });
 
-  it("can concatenate multiple number clicks", function(){
+  it("can concatenate multiple number clicks", function() {
     calculator.numberClick(3);
     calculator.numberClick(5);
     calculator.numberClick(8);
     assert.equal(358, calculator.runningTotal);
-  })
+  });
 
-  it("can chain multiple operations together", function(){
+  it("can chain multiple operations together", function() {
     calculator.numberClick(3);
-    calculator.operatorClick('*');
+    calculator.operatorClick("*");
     calculator.numberClick(5);
-    calculator.operatorClick('+');
+    calculator.operatorClick("+");
     calculator.numberClick(3);
-    calculator.operatorClick('=');
-    assert.equal(18, calculator.runningTotal)
-  })
+    calculator.operatorClick("=");
+    assert.equal(18, calculator.runningTotal);
+  });
 
-  it("clear the running total without affecting the calculation", function(){
+  it("clear the running total without affecting the calculation", function() {
     calculator.numberClick(2);
     calculator.numberClick(5);
-    calculator.operatorClick('+');
-    calculator.numberClick(3);
-    calculator.operatorClick('=');
-    calculator.operatorClick('+');
-    calculator.numberClick(8);
-    calculator.operatorClick('=');
+    calculator.operatorClick("+");
+    calculator.numberClick(9);
     calculator.clearClick();
-    assert.equal(28, calculator.runningTotal)
-  })
-
+    calculator.operatorClick("=");
+    assert.equal(25, calculator.previousTotal);
+  });
 });
